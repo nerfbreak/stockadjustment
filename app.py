7b1b4db58c90a655a40112a7f0d8bb169f4095cb
nerfbreak <rizkfirdaus@gmail.com>
2026-05-05 23:45:30 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ st.markdown("""
 hdr_col1, hdr_col2 = st.columns([5, 1])
 with hdr_col1:
     st.markdown("<div class='live-indicator'>LIVE</div>", unsafe_allow_html=True)
-    st.markdown("<h1>Compare Stock & Adjust</h1>", unsafe_allow_html=True)
+    st.markdown("<h1>Compare & Stock Adjustment</h1>", unsafe_allow_html=True)
     st.markdown("<div class='typewriter-sub'>Inspired by Kopi Mang Toni...</div>", unsafe_allow_html=True)
 st.markdown("---")
 
